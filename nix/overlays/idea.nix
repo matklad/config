85cfee4e9abba20620dc9e3064301a663207a2ac
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-09-02 11:03:26 +0300

    jbsdk

--- nix/overlays/idea.nix
+++ nix/overlays/idea.nix
@@ -1,11 +1,15 @@
 self: super:
 
 {
+  jbsdk = self.callPackage ~/config/nix/jbsdk.nix {};
+
   idea-community = let 
     version = "2017.2.3";
     sha256 = "727ba10b55c9bba9d21e1703bd86af832d05f25242efddf9dd3b99841d23d71b";
   in
-    super.idea.idea-community.overrideAttrs (attrs: rec {
+    (super.idea.idea-community.override { jdk = self.jbsdk; })
+
+    .overrideAttrs (attrs: rec {
       inherit version;
       name = "idea-community-${version}";
       src = self.fetchurl {
