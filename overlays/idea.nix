021969347a1ec3e7768d602f5b2a30961b98b00d
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-09-02 10:15:21 +0300

    Add idea overlay

---
+++ overlays/idea.nix
@@ -0,0 +1,16 @@
+self: super:
+
+{
+  idea-community = let 
+    version = "2017.2.3";
+    sha256 = "727ba10b55c9bba9d21e1703bd86af832d05f25242efddf9dd3b99841d23d71b";
+  in
+    super.idea.idea-community.overrideAttrs (attrs: rec {
+      inherit version;
+      name = "idea-community-${version}";
+      src = self.fetchurl {
+        inherit sha256;
+        url = "https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz";
+      };
+    });
+}
