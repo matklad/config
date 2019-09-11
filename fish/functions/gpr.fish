79568410ae0e2da72c5e98e1d848ba653a59fe30
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-09-11 19:39:54 +0300

    quiet

--- fish/functions/gpr.fish
+++ fish/functions/gpr.fish
@@ -1,6 +1,6 @@
 function gpr
     set -l pr $argv[1]
-    if git remote | rg upstream
+    if git remote | rg -q upstream
         git fetch upstream pull/$pr/head:pr-$pr
     else
         git fetch origin pull/$pr/head:pr-$pr
