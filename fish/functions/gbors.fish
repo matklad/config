55115b1bb9b9a7eca406cfccdd10de2118a5bd09
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-02-06 17:50:12 +0100

    Allow overriding gbors message

--- fish/functions/gbors.fish
+++ fish/functions/gbors.fish
@@ -1,5 +1,9 @@
 function gbors
     set -l message (git show -s --format=%B HEAD)
+    if set -q argv[1]
+        set message $argv
+    end
+
     set -l pr (git-hub pull new -m $message | rg '.*\[(\d+)\].*' -r '$1')
     echo "created pr #$pr"
     git-hub pull comment -m"bors r+
