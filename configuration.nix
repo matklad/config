cb0fc226bb7dfdbd8a089d2b05b1438dda34beaa
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-04-14 09:22:00 +0200

    upstream jumpapp

--- configuration.nix
+++ configuration.nix
@@ -8,25 +8,6 @@
 let
   # sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
   # unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
-  jumpapp = let
-    runtimePath = pkgs.lib.makeSearchPath "bin" (with pkgs; [ xdotool wmctrl xorg.xprop nettools perl ]);
-  in
-    pkgs.stdenv.mkDerivation rec {
-      version = "1.0";
-      name = "jumpapp-${version}";
-      src = pkgs.fetchFromGitHub {
-        owner = "matklad";
-        repo = "jumpapp";
-        rev = "d04e55af8e66087f68b9cf7817649236bf8be49b";
-        hash = "sha256:118gbi8k31y11rkgjabj7ihb9z1lfkckhvr9ww2vybk411irghj3";
-      };
-      makeFlags = [ "PREFIX=$(out)" ];
-      buildInputs = [ pkgs.perl pkgs.pandoc ];
-      postFixup = ''
-        sed -i "2 i export PATH=${runtimePath}:\$PATH" $out/bin/jumpapp
-        sed -i "2 i export PATH=${runtimePath}:\$PATH" $out/bin/jumpappify-desktop-entry
-      '';
-    };
 
   vscodeInsiders = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs(oldAttrs: rec {
     name = "vscode-insiders";
