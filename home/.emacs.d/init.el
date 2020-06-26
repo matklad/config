8667f325e968ffcb0b6952ce461b944a166a3518
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-06-26 10:22:12 +0200

    .

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -37,6 +37,17 @@
 (global-set-key (kbd "C-S-k") 'kill-whole-line)
 (global-set-key (kbd "C-a") 'mark-whole-buffer)
 
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+(global-set-key (kbd "C-/") 'comment-or-uncomment-region-or-line)
+
 (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
 (require 'package)
