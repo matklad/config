10ee55883c01132a8390a809e74cae2621a75db6
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-12-01 11:18:57 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,7 @@ in
     deadbeef-with-plugins
     filelight
     firefox-bin
-    # vivaldi
+    (vivaldi.override { proprietaryCodecs = true; enableWidevine = true;})
     gimp
     gwenview
     kitty
