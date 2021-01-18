3d3f3019ea48ff0a91c50248d6830053fb24433f
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-01-18 13:24:36 +0300

    .

--- shell.nix
+++ shell.nix
@@ -3,14 +3,23 @@ with import <nixpkgs> {};
   buildInputs = [
     pkgconfig
     python cmake
-    openssl zlib libgit2 libxml2 pcre freetype
-    xorg.libX11 libGL
+    openssl zlib libgit2 libxml2 pcre freetype fontconfig
+    xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
     ncurses
     gtk3 glib
-    vulkan-loader
+    libGL vulkan-loader
     llvmPackages_10.libclang
   ];
   shellHook = ''
-    export LD_LIBRARY_PATH="${vulkan-loader}/lib:${xlibs.libX11.out}/lib:${llvmPackages_10.libclang}/lib:${libGL}/lib:${freetype}/lib"
-  '';
+    export LD_LIBRARY_PATH="\
+${vulkan-loader}/lib:\
+${xlibs.libX11.out}/lib:\
+${xlibs.libXcursor.out}/lib:\
+${xlibs.libXrandr.out}/lib:\
+${xlibs.libXi.out}/lib:\
+${llvmPackages_10.libclang}/lib:\
+${libGL}/lib:\
+${freetype}/lib:\
+${fontconfig.lib}/lib:\
+"'';
 }
