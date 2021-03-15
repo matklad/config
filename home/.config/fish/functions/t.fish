28813b3125849f6fbd6399f72d39be954c941506
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-03-15 18:37:14 +0300

    t

--- home/.config/fish/functions/t.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function t --description alias\ time=time\ --format\ \'real\ \%es\\nuser\ \%Us\\nsys\ \ \%Ss\\nrss\ \ \%Mk\'
-	command time --format 'real %es\nuser %Us\nsys  %Ss\nrss  %Mk' $argv;
-end
