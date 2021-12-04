c558737507bcb22e0a89faf7b31435894ac82e27
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-12-04 15:29:58 +0300

    .

---
+++ hosts/Moby.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/20c4aff6-7290-4691-b380-329d04f78a74";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/0818-EB3C";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/c22f65e5-623b-4af0-b2fa-51b0a36090ea";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  nix.maxJobs = lib.mkDefault 8;
+  powerManagement.cpuFreqGovernor = "powersave";
+  services.xserver.videoDrivers = [ "modesetting" ];
+}
