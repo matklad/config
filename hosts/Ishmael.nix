e55e36e96e7f43215788756ce6cc845b0f6f35dc
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-05-17 17:04:28 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -1,35 +1,30 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
 
   networking.hostName = "Ishmael";
-  time.timeZone = "Europe/Moscow";
+  time.timeZone = "Europe/Lisbon";
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = ["wl" "amdgpu" ];
-  boot.kernelModules = [ "kvm-amd" "wl" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/04a8bc94-33a1-4fd7-b550-67e72ae1d5bc";
+      fsType = "ext4";
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/home";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/5AA0-8CE2";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/0E22-5D33";
+      fsType = "vfat";
+    };
 
   swapDevices = [ ];
 
-  hardware.video.hidpi.enable = lib.mkDefault true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  networking.useDHCP = lib.mkDefault true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = "powersave";
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.enableRedistributableFirmware = true;
 }
