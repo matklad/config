cfdd2fb4399cfa492f02e65f6be49b90a8aa90eb
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-06-26 21:34:37 +0100

    .

--- tool/src/prf.rs
+++ tool/src/prf.rs
@@ -3,7 +3,7 @@ use xshell::{cmd, Shell};
 pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
     let mut args = std::env::args();
     let _progn = args.next();
-    cmd!(sh, "perf record -F500 -g --call-graph=dwarf,65500 {args...}").run()?;
+    cmd!(sh, "perf record -F997 -g --call-graph=dwarf,65500 {args...}").run()?;
     let data = cmd!(sh, "perf script -F +pid").read()?;
     let output = "perf_profile";
     sh.write_file(output, &data)?;
