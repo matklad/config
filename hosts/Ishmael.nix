704f935342d89e680e81c5faffbfb6f72c33627a
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-07-04 10:36:07 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -17,7 +17,7 @@
     enableRedistributableFirmware = true;
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "intel" "nvidia" ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/04a8bc94-33a1-4fd7-b550-67e72ae1d5bc";
