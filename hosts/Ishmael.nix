3248ff2c2047fc91144b178df35f9d4ffcfadcfb
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-07-12 10:23:19 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -16,6 +16,11 @@
     bluetooth.enable = true;
     enableRedistributableFirmware = true;
     nvidia.modesetting.enable = true;
+    nvidia.prime = {
+      offload.enable = true;
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId  = "PCI:0:2:0";
+    };
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
   services.xserver.videoDrivers = [ "intel" "nvidia" ];
