7abbfb2a30c28009260412471b40665a2e44f99a
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-10-05 19:47:33 +0100

    .

--- tool/src/git_refresh.rs
+++ tool/src/git_refresh.rs
@@ -3,8 +3,10 @@ use xshell::{cmd, Shell};
 use crate::opt_single_arg;
 
 pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
+    let commit = opt_single_arg()?;
+
     cmd!(sh, "git fetch upstream").run()?;
-    match opt_single_arg()? {
+    match commit {
         None => cmd!(sh, "git rebase upstream/master"),
         Some(commit) => cmd!(sh, "git rebase --onto upstream/master {commit}^"),
     }
