9344e45181c7356eea95b112ee2af075d493ec4c
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-10-18 18:38:00 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -18,7 +18,7 @@
     nvidia.modesetting.enable = true;
     nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
     nvidia.prime = {
-      offload.enable = true;
+      sync.enable = true;
       intelBusId  = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
