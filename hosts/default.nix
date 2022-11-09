2e088f2b3d92bf97adeba67edddd06a53a0327a0
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-11-09 20:40:24 +0000

    .

--- hosts/default.nix
+++ hosts/default.nix
@@ -124,6 +124,7 @@
     xclip
     xorg.xkbcomp
     pciutils
+    haskellPackages.kmonad
 
     # Rust stuff
     bat
@@ -175,22 +176,29 @@
         devices = ["/dev/input/by-path/platform-i8042-serio-0-event-kbd"];
         config = ''
           (defsrc
-            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-            caps a    s    d    f    g    h    j    k    l    ;    '    ret
-            lsft z    x    c    v    b    n    m    ,    .    /    rsft
-            lctl lmet lalt           spc            ralt rmet rctl)
+            grv  1    2    3    4    5    6    7     8    9     0    -    =    bspc
+            tab  q    w    e    r    t    y    u     i    o     p    [    ]    \
+            caps a    s    d    f    g    h    j     k    l     ;    '    ret
+            lsft z    x    c    v    b    n    m     ,    .     /    rsft
+            lctl lmet lalt           spc             ralt rmet  rctl)
 
           (deflayer qwerty
-            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
-            @cap a    s    d    f    g    h    j    k    l    ;    '    ret
-            lsft z    x    c    v    b    n    m    ,    .    /    rsft
-            lctl lmet lalt           spc            ralt rmet rctl)
+            grv  1    2    3    4    5    6    7     8    9     0    -    =    bspc
+            tab  q    w    e    r    t    y    u     i    o     p    [    ]    \
+            @cap a    s    d    f    g    h    j     k    l     ;    '    ret
+            lsft z    x    c    v    b    n    m     ,    .     /    rsft
+            lctl lmet lalt           @sp             ralt rmet  rctl)
+
+         (deflayer motion
+            grv  1    2    3    4    5    6    7     8    9     0    -    =    bspc
+            tab  q    w    e    r    t    y    pgup  up   pgdn  p    [    ]    \
+            @cap a    s    d    f    g    bks  left  down right del  '    ret
+            lsft z    x    c    v    b    n    m     home end   /    rsft
+            lctl lmet lalt           @sp             ralt rmet  rctl)
 
           (defalias
-            ;; tap within 100ms for capslk, hold more than 100ms for lctl
-            cap (tap-hold 100 100 caps lctl))
+            sp (tap-hold 100 100 spc (layer-toggle motion))
+          )
         '';
       };
     };
