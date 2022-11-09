0e43f7773778497e604fedd972414a9b21f8c543
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-11-09 20:04:34 +0000

    .

--- hosts/default.nix
+++ hosts/default.nix
@@ -124,7 +124,6 @@
     xclip
     xorg.xkbcomp
     pciutils
-    haskellPackages.kmonad
 
     # Rust stuff
     bat
@@ -181,14 +180,14 @@
             caps a    s    d    f    g    h    j    k    l    ;    '    ret
             lsft z    x    c    v    b    n    m    ,    .    /    rsft
             lctl lmet lalt           spc            ralt rmet rctl)
-  
+
           (deflayer qwerty
             grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
             tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
             @cap a    s    d    f    g    h    j    k    l    ;    '    ret
             lsft z    x    c    v    b    n    m    ,    .    /    rsft
             lctl lmet lalt           spc            ralt rmet rctl)
-  
+
           (defalias
             ;; tap within 100ms for capslk, hold more than 100ms for lctl
             cap (tap-hold 100 100 caps lctl))
