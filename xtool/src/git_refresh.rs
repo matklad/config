1aea058f2cb1dd3eb721b9b18ce834effecf1d41
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-11-25 15:41:38 +0000

    .

--- xtool/src/git_refresh.rs
+++ xtool/src/git_refresh.rs
@@ -6,10 +6,12 @@ pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
     let remote =
         if cmd!(sh, "git remote").read()?.contains("upstream") { "upstream" } else { "origin" };
 
+    let branch = if cmd!(sh, "git branch").read()?.contains("master") { "master" } else { "main" };
+
     cmd!(sh, "git fetch {remote}").run()?;
     match flags.commit {
-        None => cmd!(sh, "git rebase {remote}/master"),
-        Some(commit) => cmd!(sh, "git rebase --onto {remote}/master {commit}^"),
+        None => cmd!(sh, "git rebase {remote}/{branch}"),
+        Some(commit) => cmd!(sh, "git rebase --onto {remote}/{branch} {commit}^"),
     }
     .run()?;
     Ok(())
--- xtool/src/gpr.rs
+++ xtool/src/gpr.rs
@@ -2,8 +2,12 @@ use xshell::{cmd, Shell};
 
 pub(crate) fn run(sh: &Shell) -> anyhow::Result<()> {
     let flags = xflags::parse_or_exit! { required pr: String };
+
+    let remote =
+        if cmd!(sh, "git remote").read()?.contains("upstream") { "upstream" } else { "origin" };
+
     let pr = flags.pr;
-    cmd!(sh, "git fetch upstream refs/pull/{pr}/head").run()?;
+    cmd!(sh, "git fetch {remote} refs/pull/{pr}/head").run()?;
     cmd!(sh, "git switch --detach FETCH_HEAD").run()?;
     Ok(())
 }
