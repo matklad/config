aae52417446352aadb9132266859b9272eb030f4
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-12-06 14:10:38 +0000

    .

--- hosts/default.nix
+++ hosts/default.nix
@@ -44,7 +44,6 @@ helix
     qbittorrent
     spectacle
     vscode
-    plasma-pa # volume applet for pipewire
 
     # Comms
     tdesktop
@@ -158,8 +157,22 @@ helix
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
-      pulse.enable = false;
+      pulse.enable = true;
+      media-session.config.bluez-monitor.rules = [
+        {
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+          actions = {
+            "update-props" = {
+              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+              "bluez5.msbc-support" = true;
+              "bluez5.sbc-xq-support" = true;
+            };
+          };
+        }
+        { matches = [ { "node.name" = "~bluez_input.*"; } { "node.name" = "~bluez_output.*"; } ]; }
+      ];
     };
+
     unclutter-xfixes.enable = true;
     printing.enable = true;
     earlyoom.enable = true;
