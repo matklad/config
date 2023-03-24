051c6d89ca6a0c94c99815fedf25037d14010a52
Aleksey Kladov <aleksey.kladov@gmail.com>
2023-03-24 19:39:44 +0000

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -22,7 +22,12 @@
     };
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
-  services.xserver.videoDrivers = [ "intel" "nvidia" ];
+  services.xserver = {
+    videoDrivers = [ "intel" "nvidia" ];
+    displayManager = {
+      defaultSession = "plasmawayland";
+    };
+  }
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/04a8bc94-33a1-4fd7-b550-67e72ae1d5bc";
