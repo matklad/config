e313e3a185c3c2c84169ea94a7f41d7c7782552a
Aleksey Kladov <aleksey.kladov@gmail.com>
2023-04-29 16:19:44 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -14,16 +14,16 @@
   hardware = {
     bluetooth.enable = true;
     enableRedistributableFirmware = true;
-    # nvidia.modesetting.enable = true;
-    # nvidia.prime = {
-    #   sync.enable = true;
-    #   intelBusId  = "PCI:0:2:0";
-    #   nvidiaBusId = "PCI:1:0:0";
-    # };
+    nvidia.modesetting.enable = true;
+    nvidia.prime = {
+      offload.enable = true;
+      intelBusId  = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
   services.xserver = {
-    videoDrivers = [ "intel" ];
+    videoDrivers = [ "nvidia" ];
     displayManager = {
       defaultSession = "plasmawayland";
     };
