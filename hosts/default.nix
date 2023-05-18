adeeef0076596b6025da68587d8522b4f79b0cbd
Aleksey Kladov <aleksey.kladov@gmail.com>
2023-05-18 12:02:46 +0100

    .

--- hosts/default.nix
+++ hosts/default.nix
@@ -128,17 +128,17 @@
   services = {
     emacs = {
       enable = true;
-      package = pkgs.emacs.overrideAttrs (old: {
-        pname = "emacs";
-        version = "head";
-        src = pkgs.fetchFromGitHub {
-          owner = "emacs-mirror";
-          repo = "emacs";
-          rev = "0e8d8a72284f6b3aaa1bbce73d41c7d84bbc4d3c";
-          hash = "sha256-O75pZ8fBVcM8lMOkN16D3s4h+GnK9Oi53FNEAdj2tT4=";
-        };
-        patches = [];
-      });
+      # package = pkgs.emacs.overrideAttrs (old: {
+      #   pname = "emacs";
+      #   version = "head";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "emacs-mirror";
+      #     repo = "emacs";
+      #     rev = "0e8d8a72284f6b3aaa1bbce73d41c7d84bbc4d3c";
+      #     hash = "sha256-O75pZ8fBVcM8lMOkN16D3s4h+GnK9Oi53FNEAdj2tT4=";
+      #   };
+      #   patches = [];
+      # });
     };
 
     xserver = {
