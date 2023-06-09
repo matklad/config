6c1ab5d88f1145b99c8f623a17fed6f58bcd26b5
Alex Kladov <aleksey.kladov@gmail.com>
2023-06-09 21:17:04 +0100

    .

--- hosts/Ishmael.nix
+++ hosts/Ishmael.nix
@@ -16,16 +16,16 @@
     cpu.intel.updateMicrocode = true;
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    open = true;
-    modesetting.enable = true;
-    prime = {
-      offload.enable = true;
-      intelBusId  = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
+  services.xserver.videoDrivers = [ "intel" ];
+  # hardware.nvidia = {
+  #   open = true;
+  #   modesetting.enable = true;
+  #   prime = {
+  #     offload.enable = true;
+  #     intelBusId  = "PCI:0:2:0";
+  #     nvidiaBusId = "PCI:1:0:0";
+  #   };
+  # };
 
   boot.initrd.luks.devices."nixos".device = "/dev/disk/by-uuid/2fe2808b-7e2d-44ef-8f92-9340c040d1fc";
   fileSystems."/" = {
