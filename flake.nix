d737fbfa301414f4d6d25ce68e34793e6ea8e0a0
Alex Kladov <aleksey.kladov@gmail.com>
2024-02-27 19:25:42 +0000

    .

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:K900/nixpkgs/nixos-unstable/plasma-6";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  inputs.kde2nix.url = "github:nix-community/kde2nix/main";
-  outputs = { self, nixpkgs, nixos-hardware, kde2nix }: {
+  outputs = { self, nixpkgs, nixos-hardware }: {
     nixosConfigurations = {
       Ishmael = nixpkgs.lib.nixosSystem {
          system = "x86_64-linux";
          modules = [
            ({config, pkgs, ...}: { nix.registry.nixpkgs.flake = nixpkgs; })
            ./hosts ./hosts/Ishmael.nix
-           kde2nix.nixosModules.plasma6
            nixos-hardware.nixosModules.common-gpu-nvidia-disable
          ];
       };
@@ -18,7 +16,6 @@
          modules = [
            ({config, pkgs, ...}: { nix.registry.nixpkgs.flake = nixpkgs; })
            ./hosts ./hosts/Moby.nix
-           kde2nix.nixosModules.plasma6
          ];
       };
     };
