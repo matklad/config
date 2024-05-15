10a51c22903ab4ce76f967c63f848efac8601c75
Alex Kladov <aleksey.kladov@gmail.com>
2024-05-15 11:02:35 +0100

    .

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,6 @@
 alias ls "exa -l --icons"
 alias icat "kitty +kitten icat --align left"
 bind \e\[3\;5~ kill-word
-# bind \cx kill-whole-line
 bind \cx 'fish_clipboard_copy; commandline -f kill-whole-line'
 set fish_greeting ""
 set -U fish_prompt_pwd_dir_length 0
