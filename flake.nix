78eddc6d53c1484cee575e25e807140f6d97db85
Alex Kladov <aleksey.kladov@gmail.com>
2024-05-16 12:00:26 +0100

    .

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   # inputs.nixpkgs.url = "github:matklad/nixpkgs/matklad/main";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=60cb88cc491e819c16fc579fd697d33defd2a8e3";
+  # inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=60cb88cc491e819c16fc579fd697d33defd2a8e3";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   outputs = { self, nixpkgs, nixos-hardware }: {
     nixosConfigurations = {
