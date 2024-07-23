e0a9401098eead72d3658791d7ed14e3dfbfe05f
Alex Kladov <aleksey.kladov@gmail.com>
2024-07-23 14:59:50 +0100

    .

--- gg/src/main.rs
+++ gg/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Context as _;
+use flags::Branch;
 use xshell::{cmd, Shell};
 
 mod flags {
@@ -17,7 +18,7 @@ mod flags {
             }
             /// Fuzzy switch to a different branch.
             cmd switch {
-
+                optional branch: String
             }
             /// Creates a new branch in a synced state.
             cmd branch {
@@ -96,15 +97,13 @@ fn main() -> Result {
             flags::WorktreeCmd::Clone(clone) => context.worktree_clone(&clone.remote),
         },
         flags::GgCmd::Amend(flags::Amend) => context.amend(),
-        flags::GgCmd::Switch(flags::Switch) => context.switch(),
+        flags::GgCmd::Switch(switch) => context.switch(switch.branch.as_deref()),
         flags::GgCmd::Branch(branch) => context.branch(&branch.name, branch.offline),
         flags::GgCmd::Prune(flags::Prune) => context.prune(),
         flags::GgCmd::Commit(commit) => {
             context.commit(commit.message.as_deref(), commit.branch.as_deref())
         }
-        flags::GgCmd::Uncommit(flags::Uncommit) => {
-            context.uncommit()
-        }
+        flags::GgCmd::Uncommit(flags::Uncommit) => context.uncommit(),
         flags::GgCmd::Sync(sync) => context.sync(sync.to.as_deref()),
         flags::GgCmd::Refresh(refresh) => {
             context.refresh(refresh.from.as_deref(), refresh.to.as_deref())
@@ -159,7 +158,11 @@ impl<'a> Context<'a> {
         Ok(())
     }
 
-    fn switch(&self) -> Result {
+    fn switch(&self, branch: Option<&str>) -> Result {
+        if let Some(branch) = branch {
+            cmd!(self.sh, "git switch {branch}").run()?;
+            return Ok(());
+        }
         let branches = cmd!(self.sh, "git branch").read()?;
         let branch_selected = cmd!(self.sh, "fzf --info=inline --height=~100% --reverse --tac")
             .stdin(&branches)
@@ -218,7 +221,7 @@ impl<'a> Context<'a> {
     }
 
     fn uncommit(&self) -> Result {
-        let message =  cmd!(self.sh, "git log --format=%B -n 1 HEAD").read()?;
+        let message = cmd!(self.sh, "git log --format=%B -n 1 HEAD").read()?;
         cmd!(self.sh, "git reset --mixed HEAD~").run()?;
         cmd!(self.sh, "git commit --allow-empty -m {message}").run()?;
         Ok(())
