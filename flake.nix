8a679a119fc19bb6b18e3dde11e91eb3b9f43515
Alex Kladov <aleksey.kladov@gmail.com>
2024-09-03 10:03:03 +0100

    .

--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
         url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/338859.diff";
         sha256 = "sha256-/zDD6Ic7+FVF4A7TzNj0b3oaA9lQg0c2XUhlK7qR50A=";
       }
+      {
+        url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/338326.diff";
+        sha256 = "sha256-/zDD6Ic7+FVF4A7TzNj0b3oaA9lQg0c2XUhlK7qR50A=";
+      }
     ];
     originPkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
     nixpkgs = originPkgs.applyPatches {
