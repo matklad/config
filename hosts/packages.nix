38ba058dec9978e835727cbefea91e2e6d768cae
Alex Kladov <aleksey.kladov@gmail.com>
2024-09-11 14:27:38 +0100

    .

--- hosts/packages.nix
+++ hosts/packages.nix
@@ -4,12 +4,12 @@
   (callPackage ../xtool {})
 
   # GUI
-  ((pkgs.vivaldi.overrideAttrs oldAttrs: {
+  ((pkgs.vivaldi.overrideAttrs (oldAttrs: {
     buildPhase = builtins.replaceStrings
       ["for f in libGLESv2.so libqt5_shim.so ; do"]
       ["for f in libGLESv2.so libqt6_shim.so ; do"]
       oldAttrs.buildPhase;
-  }).override {
+  })).override {
     qt5 = pkgs.qt6;
     commandLineArgs = [ "--ozone-platform=wayland" ];
     # The following two are just my preference, feel free to leave them out
