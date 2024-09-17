6f0651adb5dad70897516e14bb1291ca3f67cbee
Alex Kladov <aleksey.kladov@gmail.com>
2024-09-17 13:00:07 +0100

    .

--- hosts/default.nix
+++ hosts/default.nix
@@ -147,12 +147,7 @@
       };
     };
 
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
+    pipewire.enable = true;
 
     unclutter-xfixes.enable = true;
     printing.enable = true;
