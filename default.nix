0b7f82bda4f85ae708f4c2502edb694fc8ab8e84
Alex Kladov <aleksey.kladov@gmail.com>
2025-10-03 13:20:06 +0100

    .

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 {
   imports =
-    [ ./hardware-configuration.nix ];
+    [ /etc/nixos/hardware-configuration.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
