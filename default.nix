d0ddb8d66b41d8cecf7d3e4cd02d3f43aa4ffd7a
Alex Kladov <aleksey.kladov@gmail.com>
2025-10-03 13:46:34 +0100

    .

--- default.nix
+++ default.nix
@@ -77,11 +77,11 @@
       ninja
       python3
       (pkgs.writeShellScriptBin "nixos-pull" ''
-         set -ex
-         REV=$(git ls-remote https://github.com/matklad/config HEAD | awk '{print $1}')
-         CONFIG=$(nix-instantiate --eval -E '(builtins.fetchGit { url = "https://github.com/matklad/config.git"; rev=$REV; }).outPath' | tr -d \")
-         NIXPKGS=$(nix-instantiate --eval -E 'builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz"; }' | tr -d \")
-         sudo nixos-rebuild switch -I nixpkgs=$NIXPKGS -I nixos-config=$CCONFIG
+          set -ex
+          REV=$(git ls-remote https://github.com/matklad/config HEAD | awk '{print $1}')
+          CONFIG=$(nix-instantiate --eval -E "(builtins.fetchGit { url = \"https://github.com/matklad/config.git\"; rev=\"$REV\"; }).outPath" | tr -d \")
+          NIXPKGS=$(nix-instantiate --eval -E 'builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz"; }' | tr -d \")
+          sudo nixos-rebuild switch -I nixpkgs=$NIXPKGS -I nixos-config=$CCONFIG
       '')
   ];
 
