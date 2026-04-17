2da6c413940bcc0fe4c6b1b1bd93b527d6166899
Alex Kladov <aleksey.kladov@gmail.com>
2026-04-17 20:38:56 +0100

    .

--- make.ts
+++ make.ts
@@ -1,11 +1,31 @@
 #!/usr/bin/env -S deno run --allow-all
 import $ from "jsr:@david/dax";
 
-switch (Deno.args[0]) {
-    case "gg":
-        await $`cargo install --path tools/gg`;
-        break;
-    case "config":
-        await $`cargo install --path tools/config`;
-        break;
+async function main() {
+    if (Deno.args.length === 0) return help();
+    switch (Deno.args[0]) {
+        case "-h":
+        case "help":
+            return help();
+        case "gg":
+            await $`cargo install --path tools/gg`;
+            break;
+        case "config":
+            await $`cargo install --path tools/config`;
+            break;
+        case "code":
+            await $`cd ./tools/my-code && npm i && npm run install-extension`
+            break;
+    }
 }
+
+function help() {
+    console.log(`./make.ts
+
+gg
+config
+code
+`);
+}
+
+await main();
