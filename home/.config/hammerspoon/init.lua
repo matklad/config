24b937300726f4370cb3d29eb919f599bec97ce3
Alex Kladov <aleksey.kladov@gmail.com>
2026-09-15 10:03:20 +0100

    .

--- home/.config/hammerspoon/init.lua
+++ home/.config/hammerspoon/init.lua
@@ -1,14 +1,14 @@
 local apps =
-{
-    F1 = "Ghostty",
-    F2 = "Visual Studio Code",
-    F3 = "Safari",
-    F4 = "Slack",
-    F5 = "Spotify",
-}
+    {
+        F1 = "Ghostty",
+        F2 = "Emacs",
+        F3 = "Safari",
+        F4 = "Slack",
+        F5 = "Spotify",
+    }
 
 for key, app in pairs(apps) do
     hs.hotkey.bind({"cmd"}, key, function()
-        hs.application.launchOrFocus(app)
+            hs.application.launchOrFocus(app)
     end)
 end
